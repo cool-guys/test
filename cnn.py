76714b60e9445163fb7a1d314bda8986ec3b9c89
juhwakKim <46366467+juhwakKim@users.noreply.github.com>
2019-05-04 20:09:40 +0900

    cnn+lstm add

--- cnn.py
+++ cnn.py
@@ -9,6 +9,7 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers.convolutional import Conv2D, MaxPooling2D
 import tensorflow as tf
+import keras.backend.tensorflow_backend as K
 
 columns = ['x','y', 'label']
 j = 0
@@ -48,34 +49,34 @@ X_train, X_test, Y_train, Y_test = train_test_split(X_DATA, Y_DATA, test_size=0.
 Y_train = keras.utils.to_categorical(Y_train,num_classes=10, dtype='float32')
 
 Y_test = keras.utils.to_categorical(Y_test,num_classes=10, dtype='float32')
+with K.tf.device('/gpu:0'):
+  model = Sequential()
+  model.add(Conv2D(40, kernel_size=5, padding="same",input_shape=(28, 28, 1), activation = 'relu'))
+  model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+  model.add(Conv2D(70, kernel_size=3, padding="same", activation = 'relu'))
+  model.add(Conv2D(200, kernel_size=3, padding="same", activation = 'relu'))
+  model.add(MaxPooling2D(pool_size=(3, 3), strides=(1, 1)))
 
-model = Sequential()
-model.add(Conv2D(40, kernel_size=5, padding="same",input_shape=(28, 28, 1), activation = 'relu'))
-model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-model.add(Conv2D(70, kernel_size=3, padding="same", activation = 'relu'))
-model.add(Conv2D(200, kernel_size=3, padding="same", activation = 'relu'))
-model.add(MaxPooling2D(pool_size=(3, 3), strides=(1, 1)))
+  model.add(Conv2D(512, kernel_size=3, padding="valid", activation = 'relu'))
+  model.add(MaxPooling2D(pool_size=(3, 3), strides=(1, 1)))
+  model.add(Flatten())
+  model.add(Dense(units=100, activation='relu'  ))
+  model.add(Dropout(0.3))
 
-model.add(Conv2D(512, kernel_size=3, padding="valid", activation = 'relu'))
-model.add(MaxPooling2D(pool_size=(3, 3), strides=(1, 1)))
-model.add(Flatten())
-model.add(Dense(units=100, activation='relu'  ))
-model.add(Dropout(0.3))
+  model.add(Dense(10,activation='softmax'))
 
-model.add(Dense(10,activation='softmax'))
+  model.summary()
 
-model.summary()
+  config = tf.ConfigProto()
+  config.gpu_options.allow_growth = True
+  sess = tf.Session(config=config)
 
-config = tf.ConfigProto()
-config.gpu_options.allow_growth = True
-sess = tf.Session(config=config)
+  model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+  hist = model.fit(X_train, Y_train,
+                  batch_size=16,
+                  epochs=30,
+                  verbose=1)
 
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-hist = model.fit(X_train, Y_train,
-                 batch_size=16,
-                 epochs=30,
-                 verbose=1)
-
-score = model.evaluate(X_test, Y_test, verbose=0)
-print('Test loss:', score[0])
-print('Test accuracy:', score[1])
+  score = model.evaluate(X_test, Y_test, verbose=0)
+  print('Test loss:', score[0])
+  print('Test accuracy:', score[1])
