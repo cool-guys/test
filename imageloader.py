080526f33232c4dab4f71f9f119ec3e5b3c30765
juhwakKim <juhk1017@Naver.com>
2019-05-20 10:06:55 +0900

    imageloader add

--- imageloader.py
+++ imageloader.py
@@ -11,100 +11,121 @@ import math
 from math import floor, ceil
 from scipy.ndimage import gaussian_filter
 import time
-columns = ['x','y']
-j = 0
-x_data = []
-x_1_data = []
-y_data = []
-aug = []
-point_list = []
-augmented_images = []
-augmented_image = []
-augmented_point = []
-augmented_points = []
-rotation = 0
+import glob
 
-
-for i in range(10):
-  j = 0
-  while(os.path.exists("./DATA/Video/{}/{}train_{}".format(i,i,j))):
-    j += 1
-
-  for k in range(500):
+class data_process:
+  def __init__(self,dir):
+    self.point = []
+    self.label = []
+    self.num_dict = {}
+    self.dir = dir
+    self.images = []
+  def point_data_load(self):
+    dir_list = []
+    point_list = []
+    label_list = []
     start_time = time.time()
-    df = pd.read_csv("./DATA/Video/{}/{}train_{}".format(i,i,k%j))
-    df = df.loc[(df.x!=0) & (df.y !=0)]
-    df_img = df[['x','y']].to_numpy()
-    img = np.zeros((550, 550, 1), np.uint8)
-    img_ = np.zeros((550, 550, 1), np.uint8)
-    theta = np.random.uniform(-1,1) * np.pi
+    for dirpath, dirnames, filenames in os.walk(self.dir):
+      dir_list.append(dirnames)
+    
+    if(dir_list[0] == []):
+      for i in range(10):
+        j = 0
+        while(os.path.exists(self.dir + "/{}augs_{}.pickle".format(i,j))):
+          j += 1
+        for k in range(j):
+          df = pd.read_pickle(self.dir + '/{}augs_{}.pickle'.format(i,k))
+          points = df[['x','y']].to_numpy()
+          labels = df[['label']].to_numpy()
+          point_list.append(points)
+          label_list.append(labels)
 
-    a = np.random.uniform(1.3,2)*0.8
-    b = np.random.uniform(-1.3,1.3)*0.6
-    c = np.random.uniform(0.8,2)
-
-    #while(abs(b) < 0.5):
-      #b = np.random.uniform(-1.3,1.3)
-    if(i == 1):
-      a = np.random.uniform(1.3,2)*0.5
-      b = np.random.uniform(-1.3,1.3)*0.5
-      c = np.random.uniform(1,2)
     else:
-      if(b < 0):
-        while(a-b > 3.2*0.8):
-          a = np.random.uniform(1.3,2)*0.8
-          b = np.random.uniform(-1.3,1.3)*0.6
-          while(c < 1.5):
-            c = np.random.uniform(1,2)
-      else:
-        while(a-b < 1.5*0.8):
-          a = np.random.uniform(1.3,2)*0.8
-          b = np.random.uniform(-1.3,1.3)*0.6
-          while(c < 1.5):
-            c = np.random.uniform(1,2)
-
-
-
-    Q = np.array([[np.cos(theta/18),-np.sin(theta/18)],[np.sin(theta/18),np.cos(theta/18)]])
-    R = np.array([[a,b],[0,c]])
-    A = np.matmul(Q,R)
-    #A = np.array([[1+a,0],[0,1+a]])
-    for l in range(len(df_img)):
-      if(k <= 100):
-        point_ = df_img[l]
-      else:
-        point_ = np.matmul(Q,df_img[l])
-        point_ = np.around(point_)
-        point_ = point_.astype(int)
-      if(point_[0] > 1000 or point_[1] > 1000 ):
-        #print(A)
-        print(i)
-        print(k)
-        print('asd',A)
-
-      augmented_points.append(point_)
-
-    df = pd.DataFrame(augmented_points, columns= ['x','y'])
-    #df = df.loc[(df.x!=400) & (df.y !=400)]
-    df_img_x = df[['x']].to_numpy()
-    df_img_x_mean = np.mean(df_img_x)
-    x_dif = int(400-df_img_x_mean)
-    df_img_x += x_dif
-    #print('x',df_img_x)
-    df_img_y = df[['y']].to_numpy()
-    df_img_y_mean = np.mean(df_img_y)
-    y_dif = int(400-df_img_y_mean)
-    df_img_y +=y_dif
-
-    df_img = np.concatenate((df_img_x,df_img_y), axis=1)
-    df_img = np.rint(df_img)
-    df_img = df_img.astype(int)
-    dataframe = pd.DataFrame(df_img, columns= ['x','y'])
-    dataframe['label'] = i
-    dataframe.to_pickle("./DATA/test/{}augs_{}".format(i,k))
-    augmented_images = []
-    augmented_image = []
-    augmented_point = []
-    augmented_points = []
+      for direct in dir_list[0]:
+        folder = self.dir + '/' + direct
+        for files in os.listdir(folder):
+          if(files[-1] == 'v'):
+            df = pd.read_csv(folder + '/' + files)
+            df = df.loc[(df.x!=0) & (df.y !=0)]
+            points = df[['x','y']].to_numpy()
+            labels = df[['label']].to_numpy()
+            point_list.append(points)
+            label_list.append(labels)
+    
+    Point_DATA = np.array(point_list)
+    Label_DATA = np.array(label_list)
+    
+    for i in range(np.size(Label_DATA,0)):
+      Label_DATA[i] = np.unique(Label_DATA[i],axis=0)
+    Label_DATA = Label_DATA.reshape((np.size(Label_DATA,0),1))
+    unique, counts = np.unique(Label_DATA, return_counts=True)
+    #print(unique)
+    unique = ['0','1','2','3','4','5','6','7','8','9']
+    num_dict = dict(zip(unique, counts))
     print("--- %s seconds ---" %(time.time() - start_time))
+
+    self.point = Point_DATA
+    self.label = Label_DATA
+    self.num_dict = num_dict
+
+
+  def image_make(self):
+
+    for i in range(np.size(self.point,0)):
+      img = np.zeros((800, 800, 1), np.uint8)
+      for k in range(len(self.point[i])):
+        if(k != len(self.point[i])-1):
+          if(self.label[i][0][0][0] == 1):
+            cv2.line(img, (self.point[i][k][0],self.point[i][k][1]), (self.point[i][k+1][0],self.point[i][k+1][1]), (255,255,255), 3)
+          else:
+            cv2.line(img, (self.point[i][k][0],self.point[i][k][1]), (self.point[i][k+1][0],self.point[i][k+1][1]), (255,255,255), 10)
+      ret,thresh = cv2.threshold(img,127,255,0)
+      contours,hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, 2)
+      cnt = contours[0]
+      x,y,w,h = cv2.boundingRect(cnt)
+      if(x < 20 or y < 20):
+        img = img[y:y+h+20,x:x+w+20]
+      else:
+        img = img[y-20:y+h+20,x-20:x+w+20]
+      img = cv2.flip(img, 1)
+      img = cv2.resize(img,(28,28),interpolation=cv2.INTER_AREA)
+      #print(self.label[i][0][0][0])
+      j = 0
+      while(os.path.exists('./DATA/image/{}img_{}.jpg'.format(self.label[i][0][0][0],j))):
+        if(j < self.num_dict[str(self.label[i][0][0][0])]):
+          j += 1
+      cv2.imwrite('./DATA/image/{}img_{}.jpg'.format(self.label[i][0][0][0],j), img)
+
+  def image_read(self):
+    images = []
+    labels = []
+    for files in os.listdir('./DATA/image'):
+      img = cv2.imread('./DATA/image/' + files, cv2.IMREAD_GRAYSCALE)
+      images.append(img)
+      labels.append(files[0])
+    images = np.array(images)
+    images = np.reshape(images,(-1,28,28,1))
+    images = images/255
+    self.images = images
+    self.label = np.array(labels)
+  
+  def data_shuffle(self,point_only= False,image_only= False):
+    if(point_only):
+      indices = np.arange(self.point.shape[0])
+      np.random.shuffle(indices)
+      self.point = self.point[indices]
+      self.label = self.label[indices]
+    elif(image_only):
+      indices = np.arange(self.images.shape[0])
+      np.random.shuffle(indices)
+      self.images = self.images[indices]
+      self.label = self.label[indices]
+    else:
+      indices = np.arange(self.point.shape[0])
+      np.random.shuffle(indices)
+      self.point = self.point[indices]
+      self.images = self.images[indices]
+      self.label = self.label[indices]
+
+
 
